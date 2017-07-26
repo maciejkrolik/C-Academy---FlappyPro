5bdd6a4017ceba0dcf63dec53dfbe68127ced619
Maciej Królik <maciejkrolik@outlook.com>
2017-07-26 21:23:23 +0200

    Updated shop, added homebutton

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -9,7 +9,10 @@ public class GameControl : MonoBehaviour
     public static GameControl instance;
     public GameObject gameOverText;
     public GameObject shopButton;
+    public GameObject homeButton;
+    public Text highScoreText;
     public Text scoreText;
+
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
 
@@ -49,8 +52,13 @@ public class GameControl : MonoBehaviour
 
     public void BirdDied()
     {
+        if (PlayerPrefs.GetInt("HighScore", 0) < score)
+            PlayerPrefs.SetInt("HighScore", score);
         gameOverText.SetActive(true);
         shopButton.SetActive(true);
+        homeButton.SetActive(true);
+        highScoreText.text = "Highscore: " + PlayerPrefs.GetInt("HighScore", 0).ToString();
+        highScoreText.gameObject.SetActive(true);
         gameOver = true;
     }
 
@@ -59,15 +67,17 @@ public class GameControl : MonoBehaviour
         SceneManager.LoadScene(name, LoadSceneMode.Additive);
         gameOverText.SetActive(false);
         shopButton.SetActive(false);
+        homeButton.SetActive(false);
     }
 
     public void UnloadScene_Shop()
     {
         gameOverText.SetActive(true);
         shopButton.SetActive(true);
+        homeButton.SetActive(true);
     }
 
-    public void LoadScene_flappy(string name)
+    public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
     }
